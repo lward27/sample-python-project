53ab30a91a41e9f0bb883b4596a1557948338d48
Lucas WARD <lward@ipponusa.com>
2022-10-24 10:02:02 -0400

    Fixed build script to separate build requirements into a different venv

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -e
 set -x
 
 python3 -m venv dist/venv
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+deactivate
 source dist/venv/bin/activate
 python3 -m pip install -r build_requirements.txt
 shiv --site-packages venv/lib/python3.9/site-packages \
